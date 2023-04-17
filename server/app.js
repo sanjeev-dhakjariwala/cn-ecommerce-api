be7aa7fa96b292f6d5c5505d873cac586779bc9d
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2023-04-18 00:48:48 +0530

    Comments added

--- server/app.js
+++ server/app.js
@@ -5,9 +5,12 @@ const bodyParser = require("body-parser");
 const mongoDB = require("../db/db");
 
 app.use(cors());
+//contecting to MongoDB that is getting imported above
 mongoDB();
 
+//using bodyParser to parse json request body
 app.use(bodyParser.json({ limit: "50mb" }));
+//using bodyParser to parse urlencoded request body
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json({ limit: "50mb" }));
 
--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const app = require("./app");
 require("dotenv").config();
+//using colors npm module to add colors to console
 const colors = require("colors");
 
+//taking PORT number from env file or 4000 by default
 const PORT = process.env.PORT || 4000;
 
+//App is listening on port that is present in the env file or 4000 by default
 app.listen(PORT, () => {
   console.log(colors.blue(`APP IS LISTENING on PORT ${PORT}`));
 });
