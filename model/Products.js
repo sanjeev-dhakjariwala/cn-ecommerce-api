7781c16c24e3979294154257f2b3b9f4767e9e05
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2023-04-22 01:29:32 +0530

    time stamps added

--- model/Products.js
+++ model/Products.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
   },
-  quantity: {
-    type: Number,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const productModel = mongoose.model("Product", productSchema);
 
