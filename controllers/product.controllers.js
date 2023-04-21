1a99b790f2c888a730f3061c8a64c480a94fabd2
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2023-04-22 01:17:15 +0530

    All Routes implemeted

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -1,15 +1,19 @@
 const Product = require("../model/Products");
-const createProduct = async(req, res) => {
+
+//Adding product
+const createProduct = async (req, res) => {
   const body = req.body.product;
   console.log(req.body);
   if (body) {
     const addProduct = await Product.create(body);
     res.status(200).send({
-      sucess: true,
-      message: "Product added successfully",
-      product: {
-        name: body.name,
-        quantity: body.quantity,
+      data: {
+        sucess: true,
+        message: "Product added successfully",
+        product: {
+          name: body.name,
+          quantity: body.quantity,
+        },
       },
     });
   } else {
@@ -19,40 +23,73 @@ const createProduct = async(req, res) => {
     });
   }
 };
+//Getting all products
+const getProduct = async (req, res) => {
+  const products = await Product.find();
+  if (products) {
+    res.send({
+      data: {
+        success: true,
+        products,
+      },
+    });
+  } else {
+    res.send({
+      success: false,
+      message: "No Products found",
+    });
+  }
+};
+//Delete product by id
+const deleteProduct = async (req, res) => {
+  const id = req.body.id;
+  const product = await Product.findByIdAndDelete(id);
 
-const getProduct = async(req, res) => {
-    const products = await Product.find();
-    if(products){
-        res.send({
-            success: true,
-            products
-        });
-    }else{
-        res.send({
-            success: false,
-            message: "No Products found"
-        })
-    }
+  if (product) {
+    res.send({
+      data: {
+        success: true,
+        product,
+      },
+    });
+  } else {
+    res.send({
+      success: false,
+      message: "Give proper id",
+    });
+  }
 };
 
-const deleteProduct = async(req, res) =>{
-    const id = req.body.id;
-    const product = await Product.findByIdAndDelete(id);
+//update product quantity
 
-    if(product){
-        res.send({
-            success: true,
-            product
-        })
-    }else{
-        res.send({
-            success: false,
-            message: "Give proper id"
-        })
-    }
-}
+const updateProduct = async (req, res) => {
+    console.log(req.body);
+  const quantity = parseInt(req.body.quantity);
+  const id = req.body.id;
+  if (quantity) {
+    const product = await Product.findById(id);
+    console.log(id);
+    product.quantity = parseInt(product.quantity) + quantity;
+    await Product.findByIdAndUpdate(id,product);
+    res.send({
+      data: {
+        success: true,
+        message: "Product updated Successfully",
+        product,
+      },
+    });
+  } else {
+    res.send({
+      data: {
+        success: false,
+        message: "Product not found",
+      },
+    });
+  }
+};
 module.exports = {
   createProduct,
   getProduct,
-  deleteProduct
+  deleteProduct,
+  updateProduct,
 };
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const {
   createProduct,
   getProduct,
-  deleteProduct
+  deleteProduct,
+  updateProduct
 } = require("../controllers/product.controllers");
 const router = express.Router();
 
 router.get("/",getProduct);
 router.post("/create",createProduct);
 router.delete("/:id",deleteProduct);
+router.post("/:id/update_quantity",updateProduct)
 
 module.exports = router;
